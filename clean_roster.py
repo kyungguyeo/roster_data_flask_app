3466e1a613993a466a6a45e939a3d5d2a098088e
Johnny Yeo <johnnyy@youthtruthsurvey.org>
2018-05-03 10:58:26 -0700

    pushing last changes from a millioin years ago

--- clean_roster.py
+++ clean_roster.py
@@ -1,26 +1,10 @@
 import sys
 import pandas as pd
-import os
-import argparse
-
 
 '''
 Take a roster file and 1) Normalize cases, 2)Check for duplicates, 3) Check to make sure that Teacher IDs and
 teacher names form a 1:1 relationship and 4) summarize data to see if certain classes have too few students
 '''
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-o', '--outDir', metavar='outDir',
-                    help='Directory for files to be output to (default: ./)', required=False)
-
-parser.add_argument('-i', '--inFile', nargs = '+', metavar='inFile',
-                    help='File to be read in', required=True)
-
-parser.add_argument('-ignorePeriod','--ignorePeriod', dest='ignorePeriod', action = 'store_true',
-                    help='Does not include period with course (default: False)', required=False)
-
-parser.add_argument('-remove','--remove', dest='remove', action = 'store_true',
-                    help='Automatically removes courses with fewer than 5 students (default: False)', required=False)
 
 
 def removeSpace(roster_df):
@@ -33,7 +17,6 @@ def removeSpace(roster_df):
     roster_df['teacherlast'] = roster_df['teacherlast'].map(str.strip)
     roster_df['class'] = roster_df['class'].map(str.strip)
     roster_df['subject'] = roster_df['subject'].map(str.strip)
-
     return roster_df
 
 def normCases(roster_df):
@@ -45,10 +28,8 @@ def normCases(roster_df):
 
     # Each column
     for col in roster_df:
-
         # Each item
         for ix, item in roster_df[col].iteritems():
-
             # If string, make all upper case
             if isinstance(item,str):
                 parts = item.split(' ')
@@ -58,30 +39,110 @@ def normCases(roster_df):
                         new_parts.append(part.title())
                     else:
                         new_parts.append(part.upper())
-
                 roster_df[col][ix] = ' '.join(new_parts)
-
     return roster_df
 
 
-def checkMissing(roster_df,dropPeriod):
+def dropDuplicates(roster_df):
+    '''
+    Drop duplicates and return new de-duped file
+    :param roster_df: (DataFrame)
+    :return: (DataFrame) de-duped
+    '''
+
+    length_before = len(roster_df)
+    roster_df = roster_df.drop_duplicates(keep='last')
+    length_dedup = len(roster_df)
+
+    if length_dedup != length_before:
+        print >> sys.stdout, "Removed %d duplicate values from roster data" % (length_before - length_dedup)
+    return roster_df
+
+
+def checkMissing(roster_df, dropPeriod):
     '''
     Determine if the roster file has missing values
     :param roster_df: (DataFrame)
     :param dropPeriod: (bool) whether we can ignore the period section
     '''
-    if dropPeriod == True:
+    if dropPeriod:
         roster_df = roster_df[['StudentID', 'linked_grade', 'teacherfirst', 'teacherlast', 'TeacherID', 'class',
                                'subject','schoolname','email']]
-
-    needQuit = False
+    issue = []
     for index, row in roster_df.iterrows():
         hasNulls = pd.isnull(row).tolist()
         if True in hasNulls:
-            print "\n--Missing Info: Check row %i in roster file" %(index)
-            needQuit = True
-    if needQuit:
-        sys.exit(1)
+            issue.append("\n--Missing Info: Check row %i in roster file" %(index))
+    return issue
+
+
+def check_issues(roster_df):
+    '''
+    Check to make sure teacher name and Teacher ID is 1:1
+    :param roster_df:
+    :return: (string) if there is an issue
+    '''
+
+    issue = []
+
+    # Group by name and see if multiple IDs
+    teach_names = []
+    roster_grouped_teachname = roster_df.groupby(['teachername'])
+    for teachname, otherinfo in roster_grouped_teachname:
+        id_list = list(otherinfo['TeacherID'].unique())
+        id_list = [str(x) for x in id_list]
+        if len(id_list) > 1:
+            ids = ', '.join(id_list)
+            teach_names.append([teachname, ids])
+
+    # Group by ID and see if multiple names
+    teach_ids = []
+    roster_grouped_teachid = roster_df.groupby(['TeacherID'])
+    for teachid, otherinfo in roster_grouped_teachid:
+        name_list = list(otherinfo['teachername'].unique())
+        if len(name_list) > 1:
+            names = ', '.join(name_list)
+            teach_ids.append([str(teachid), names])
+
+    # Group by Student and check if multiple grades
+    student_ids = []
+    roster_grouped_studentid = roster_df.groupby(['StudentID'])
+    for studid, otherinfo in roster_grouped_studentid:
+        grade_list = list(otherinfo['linked_grade'].unique())
+        if len(grade_list) > 1:
+            grade_list = [str(x) for x in grade_list]
+            grades = ', '.join(grade_list)
+            student_ids.append([str(studid), grades])
+
+    # Group by course and check if multiple subjects
+    courses = []
+    roster_grouped_course = roster_df.groupby(['coursename'])
+    for course, otherinfo in roster_grouped_course:
+        subj_list = list(otherinfo['subject'].unique())
+        if len(subj_list) > 1:
+            subj_list = [str(x) for x in subj_list]
+            subjects = ', '.join(subj_list)
+            courses.append([str(course), subjects])
+
+    return teach_names, teach_ids, student_ids, courses
+
+
+def less_five(roster_df):
+    '''
+    Group roster data df by teacher and coursename to check that all classes have at least 5 students to survey
+    :return: issue
+    '''
+    issue = []
+
+    # Group by teachername and coursename to determine which courses have fewer than 5 students
+    r_grouped = roster_df.groupby(['teachername', 'coursename']).agg({'StudentID': pd.Series.nunique})
+    r_grouped = r_grouped.rename(columns={'StudentID': 'Enrollment'})
+    r_grouped = r_grouped.sort_values(by='Enrollment')
+    if min(r_grouped['Enrollment'].tolist()) < 5:
+        issue = [[x, y[0], y[1]] for x, y in zip(r_grouped[r_grouped['Enrollment'] <5]['Enrollment'].values,
+                                                 r_grouped[r_grouped['Enrollment'] < 5].index.tolist())]
+    return issue
+
 
 def formCourseName(roster_df):
     '''
@@ -102,236 +163,74 @@ def formCourseName(roster_df):
     roster_df['coursename'] = coursenames
     return roster_df
 
-def check_issues(roster_df):
+
+def summarize(roster_df, summ_name, to_csv=False):
     '''
-    Check to make sure teacher name and Teacher ID is 1:1
-    :param roster_df:
-    :return: (string) if there is an issue
-    '''
-
-    issue = ''
-
-    # Group by name and see if multiple IDs
-    teach_names = []
-    roster_grouped_teachname = roster_df.groupby(['teachername'])
-    for teachname, otherinfo in roster_grouped_teachname:
-        id_list = list(otherinfo['TeacherID'].unique())
-        id_list = [str(x) for x in id_list]
-        if len(id_list) > 1:
-            ids = ','.join(id_list)
-            teach_names.append(teachname + ' (' + ids + ')')
-
-    # Group by ID and see if multiple names
-    teach_ids = []
-    roster_grouped_teachid = roster_df.groupby(['TeacherID'])
-    for teachid, otherinfo in roster_grouped_teachid:
-        name_list = list(otherinfo['teachername'].unique())
-        if len(name_list) > 1:
-            names = ','.join(name_list)
-            teach_ids.append(str(teachid) + ' (' + names + ')')
-
-    # Group by Student and check if multiple grades
-    student_ids = []
-    roster_grouped_studentid = roster_df.groupby(['StudentID'])
-    for studid, otherinfo in roster_grouped_studentid:
-        grade_list = list(otherinfo['linked_grade'].unique())
-        if len(grade_list) > 1:
-            grade_list = [str(x) for x in grade_list]
-            grades = ','.join(grade_list)
-            student_ids.append(str(studid) + ' (' + grades + ')')
-
-    # Group by course and check if multiple subjects
-    courses = []
-    roster_grouped_course = roster_df.groupby(['coursename'])
-    for course, otherinfo in roster_grouped_course:
-        subj_list = list(otherinfo['subject'].unique())
-        if len(subj_list) > 1:
-            subj_list = [str(x) for x in subj_list]
-            subjects = ','.join(subj_list)
-            courses.append(str(course) + ' (' + subjects + ')')
-
-    if teach_names:
-        issue += '\nThe following teachers have multiple IDs: {}'.format(', '.join(teach_names))
-
-    if teach_ids:
-        issue += '\nThe following teacher IDs have multiple names: {}'.format(', '.join(teach_ids))
-
-    if student_ids:
-        issue += '\nThe following students have multiple grades: {}'.format(', '.join(student_ids))
-
-    if courses:
-        issue += '\nThe following course have multiple subjects: {}'.format(', '.join(courses))
-
-
-    return issue
-
-
-def dropDuplicates(roster_df):
-    '''
-    Drop duplicates and return new de-duped file
-    :param roster_df: (DataFrame)
-    :return: (DataFrame) de-duped
+    Summarize the number of students in each class
+    :param roster_df: (DataFrame) roster file with the following headers: StudentID,linked_grade,teacherfirst,
+                                  teacherlast,TeacherID,subject,teachername,coursename
+    :return: summarized roster DataFrame
     '''
 
-    length_before = len(roster_df)
-    roster_df = roster_df.drop_duplicates(take_last=True)
-    length_dedup = len(roster_df)
+    # Group by teachername and coursename to determine which courses have fewer than 5 students
+    r_grouped = roster_df.groupby(['teachername','coursename']).agg({'StudentID':pd.Series.nunique})
+    r_grouped = r_grouped.rename(columns = {'StudentID': 'Enrollment'})
+    r_grouped = r_grouped.sort_values(by='Enrollment')
 
-    if length_dedup != length_before:
-        print >> sys.stdout, "Removed %d duplicate values from roster data" % (length_before - length_dedup)
+    # Print if explicitly called
+    if to_csv:
+        r_grouped.to_csv(summ_name)
+    return r_grouped
 
-    return roster_df
 
-
-def clean(roster_df,ignorePeriod,clean_name,summ_name,remove):
+def data_check(roster_df, dropPeriod):
     '''
-    Check for the following:
+    Check for and clean up the following:
         - Missing values in any column
         - Make sure teacher names, class names, class periods, and subject are all same case (capitalized lower case)
         - Drop any duplicate rows
-        - Output warning if multiple IDs for same teacher first, last name
-        - Stop script if same teacher ID has multiple teacher first, last name combinations
-
-    :param roster_df: (DataFrame) roster file with the following headers (order matters, names do not for input):
-                       Student ID,Grade Level,Teacher First,Teacher Last,Teacher ID,Class Name,Class Period,
-                       Subject,School Name,Teacher Email
-    :param ignorePeriod (bool) whether to ignore the Period column when making the coursename field
-    :return: cleaned DataFrame file
+    Outputs error for the following:
+        - Multiple IDs for same teacher first, last name
+        - Same teacher ID has multiple teacher first, last name combinations
+        - Less than 5 students being surveyed in the class
+    :param roster_df:
+    :return: issue
     '''
+    issues = {}
 
     # Roster headers for clean file
     roster_headers = ['StudentID', 'linked_grade', 'teacherfirst', 'teacherlast', 'TeacherID', 'class',
-                      'period', 'subject','schoolname','email']
+                      'period', 'subject', 'schoolname', 'email']
 
-    print roster_df.columns
     # Rename columns
     roster_df.columns = roster_headers
-
-   # Check for missing values
-    checkMissing(roster_df,ignorePeriod)
-
-
-    # Remove any white space
-    roster_df = removeSpace(roster_df)
-
-    # Normalize cases
-    roster_df = normCases(roster_df)
-
-    # Get ride of duplicates
-    roster_df = dropDuplicates(roster_df)
-
-    failIssues = []
 
     # Create full teacher name and select only teachername and teacher id columns
     roster_df['teachername'] = roster_df['teacherfirst'] + " " + roster_df['teacherlast']
 
     # If necessary, concatenate class and period
-    if ignorePeriod:
+    if dropPeriod:
         roster_df['coursename'] = roster_df['class']
     else:
         roster_df = formCourseName(roster_df)
 
-    #Check 1:1
-    check_df = roster_df[['teachername','TeacherID','StudentID','linked_grade','coursename','subject']]
-    issues = check_issues(check_df)
-    if issues:
-        failIssues.append(issues)
+    # Check for missing values
+    issues["missing"] = checkMissing(roster_df, dropPeriod)
 
-    # Apply custom formatting
-    #if custom:
-    #    roster_df = custom_format_hth(roster_df)
+    # Check for any 1:1 issues
+    issues["teachmultids"], issues["idmultteach"], issues["stumultgrades"], issues["coursemultsubj"] = check_issues(roster_df)
 
-    if failIssues:
-        print >> sys.stdout, "The following issues were found in the roster data:\n {}".format('\n'.join(failIssues))
-        return "The following issues were found in the roster data:\n {}".format('\n'.join(failIssues))
-    else:
-        #Drop period column
-        roster_df = roster_df[['StudentID', 'linked_grade', 'teacherfirst', 'teacherlast', 'teachername', 'TeacherID',
-                               'coursename','subject','schoolname']]
-        roster_df, less_five = summarize(roster_df,summ_name,remove)
-        if not less_five.empty:
-            return "\nThe following courses have too few students:\n%s" % less_five
-        else:
-            try:
-                roster_df.to_csv(clean_name,index=False)
-                return ''
-            except IOError as e:
-                print >> sys.stderr, '\n**ERROR - Cant write to {}. Try closing the file if open.'.format(clean_name)
-                return '\n**ERROR - Cant write to {}. Try closing the file if open.'.format(clean_name)
+    # Check for less than 5 students issues
+    issues["lessfive"] = less_five(roster_df)
 
-def summarize(roster_df,summ_name,remove):
-    '''
-    Summarize the number of students in each class
+    if not issues:
+        # Remove any white space
+        roster_df = removeSpace(roster_df)
 
-    :param roster_df: (DataFrame) roster file with the following headers: StudentID,linked_grade,teacherfirst,
-                                  teacherlast,TeacherID,subject,teachername,coursename
-    :return: summarized roster DataFrame
-    '''
-    less_five = pd.DataFrame()
+        # Normalize cases
+        roster_df = normCases(roster_df)
 
-    # Group by teachername and coursename to determine which courses have fewer than 5 students
-    r_grouped = roster_df.groupby(['teachername','coursename']).agg({'StudentID':pd.Series.nunique})
-    r_grouped = r_grouped.rename(columns = {'StudentID': 'Enrollment'})
-    r_grouped = r_grouped.sort('Enrollment')
-    if min(r_grouped['Enrollment'].tolist()) < 5:
-        print >> sys.stdout, "\nThe following courses have too few students:\n"
-        less_five = r_grouped[r_grouped['Enrollment']<5]
-        print >> sys.stdout, less_five
+        # Get rid of duplicates
+        roster_df = dropDuplicates(roster_df)
 
-        # If selected, automatically remove those course with fewer than 5 studnets
-        if remove:
-            print 'Removing courses above from roster file...'
-            rows_remove = []
-            # Loop through main roster file
-            for ix, mainrow in roster_df.iterrows():
-                # Loop through dataframe with courses to remove where the index is a tuple of (teacher,course)
-                for index, row in less_five.iterrows():
-                    if mainrow['coursename'] == index[1] and mainrow['teachername'] == index[0]:
-                        rows_remove.append(ix)
-            # Only keep those indeces not in the identified rows to remove
-            roster_df = roster_df[-(roster_df.index.isin(rows_remove))]
-
-    # Print summary
-    if less_five.empty:
-        r_grouped.to_csv(summ_name)
-
-    return roster_df, less_five
-
-if __name__ == "__main__":
-
-    args = parser.parse_args()
-
-    # Save in cwd if no output directory specified
-    if (args.outDir and not os.path.exists(args.outDir)) or not args.outDir:
-        print "No output path or doesn't exist -- will save file here"
-        args.outDir = os.getcwd()
-
-    for infile in args.inFile:
-        if not os.path.isfile(infile):
-            raise IOError('The roster file {} does not exist.'.format(infile))
-
-        try:
-            roster = pd.read_csv(infile)
-        except:
-            raise IOError("The roster file {} could not be opened".format(infile))
-
-
-        path_noext = os.path.splitext(infile)[0]
-        filename = os.path.basename(path_noext)
-        print >> sys.stdout, "\n******Cleaning " + filename + "******\n"
-        clean_name = filename + '_clean.csv'
-        summ_name = filename + '_summary.csv'
-
-        clean_name = os.path.join(args.outDir,clean_name)
-        summ_name = os.path.join(args.outDir,summ_name)
-
-        # Output clean version
-        clean(roster,args.ignorePeriod,clean_name,summ_name,args.remove)
-
-
-
-
-
-
-
-
+    return issues, roster_df
--- hello.py
+++ hello.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
 import pandas as pd
 import os
-from clean_roster import clean
-from io import StringIO
-import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEBase import MIMEBase
-from email import encoders
+from clean_roster import data_check, summarize
 import json
+import dropbox
+
+UPLOAD_FOLDER = "~/uploads/"
+ALLOWED_EXTENSIONS = set(['csv'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -20,63 +19,28 @@ def index():
 def upload_file():
     if request.method=='POST':
         f = request.files['file']
-        roster = pd.read_csv(f.stream)
+        roster_df = pd.read_csv(f.stream)
         path_noext = os.path.splitext(f.filename)[0]
         f_name = os.path.basename(path_noext)
         clean_name = f_name + '_clean.csv'
         summ_name = f_name + '_summary.csv'
-        message = clean(roster,False,clean_name,summ_name,False)
-        if message:
-            return render_template('no_good.html', feedback = message)
+        issues, roster_df = data_check(roster_df,False)
+        if any(issues.values()):
+            return render_template('no_good.html', feedback = issues)
         else:
-            return render_template('success.html', email_func = email_youthtruth)
-@app.route('/email', methods = ['GET', 'POST'])
-def email_youthtruth():
-    ##Attach Email
-    email_data = json.loads(open('/Users/johnnyyeo/.credentials/email_creds.json').read())
-    fromaddr = email_data['user']
-    toaddr = ['johnnyy@youthtruthsurvey.org']
+            return render_template('success.html', email_func = dbx)
+
+@app.route('/dbx', methods = ['GET', 'POST'])
+def dbx(): #drops to /Apps/RosterData/roster_data_drop
+    db = dropbox.Dropbox('PSGsZQnzMeAAAAAAAAAAtS5tk1W57GHwbVp0ycdbF7wXTKXFlal3hTdstseeKNay')
     for file in os.listdir(os.getcwd()):
-        if ('.csv' in file) and ('_clean' in file):
-            clean_file = file
-        elif ('.csv' in file) and ('_summary' in file):
-            summary_file = file
-    msg = MIMEMultipart()
-    msg['From'] = fromaddr
-    msg['To'] = ", ".join(toaddr)
-    msg['Subject'] = "New Roster Data!"
-    body = """
-        <html>
-            <head></head>
-            <body>
-                Ahoy! New Roster Data For you!
-            </body>
-        </html>
-        """
-    msg.attach(MIMEText(body, 'html'))
-    attachment = open(clean_file, 'rb')
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename = %s" % clean_file)
-    msg.attach(part)
+        if ("_clean.csv" in file) or ("_summary.csv" in file):
+            with open(file, "rb") as f:
+                db.files_upload(f.read(), '/roster_data_drop/' + f.name, mute = True)
+    return render_template('dropbox.html')
 
-    attachment = open(summary_file, 'rb')
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename = %s" % summary_file)
-    msg.attach(part)
-
-    ##Send Email
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(fromaddr, email_data['pass'])
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
-    server.quit()
-
-    return render_template('email.html')
+@app.route('/dwnld', methods = ['GET'])
+def dwnld(): #downloads _summary file to cur dir
 
 if __name__ == '__main__':
     app.run(debug = True)
